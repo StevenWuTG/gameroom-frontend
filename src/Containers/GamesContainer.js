015186f7aa1085eea93e07f0fea5e6b291d7b204
StevenWuTG <stevenwu91@icloud.com>
2021-01-18 16:34:03 -0500

    final api hide

--- src/Containers/GamesContainer.js
+++ src/Containers/GamesContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import API_KEY from '../Api_keys2'
+
+
 
 export class GamesContainer extends Component {
+
+    componentDidMount(){
+        console.log("api_key", API_KEY)
+    }
     render() {
         return (
             <div>
